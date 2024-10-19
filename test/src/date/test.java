87ed0cb85d8e21cea9845b29c9640ace569f6415
JavaAndWin <sliang528@gmail.com>
2024-10-19 21:16:21 +0800

    Add files via upload

---
+++ test/src/date/test.java
@@ -0,0 +1,11 @@
+package date;
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString());
+
+    }
+}
