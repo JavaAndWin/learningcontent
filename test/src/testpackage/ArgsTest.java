ac72dc8822569c84a3dc051d50ad6c1c00bd51c1
JavaAndWin <sliang528@gmail.com>
2024-10-30 23:51:27 +0800

    10.30 Stream

---
+++ test/src/testpackage/ArgsTest.java
@@ -0,0 +1,16 @@
+package testpackage;
+
+public class ArgsTest {
+    public static void main(String[] args) {
+        System.out.println(getSum(1,2,3,4,5));
+    }
+    //本质是数组，形参只能有一个可变参数，多个参数中放末尾
+    public static int getSum(int...args) {
+
+        int sum = 0;
+        for (int i : args) {
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ test/src/testpackage/streamtest/Test.java
@@ -0,0 +1,18 @@
+package testpackage.streamtest;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        IntStream stream =Arrays.stream(arr).filter(s->s!=2);
+        IntStream stream1 =stream.filter(s->s==3);
+        //已使用的流无法再使用
+        IntStream stream2 =stream.filter(s->s==3);
+
+        stream1.forEach(System.out::println);
+        stream2.forEach(System.out::println);
+    }
+}
