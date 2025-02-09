775a2695a6d30362ba7dd7df88ce6d5f2f70710a
JavaAndWin <sliang528@gmail.com>
2025-02-09 10:43:02 +0800

    new test

---
+++ test/src/HUAWEIOD/T01.java
@@ -0,0 +1,17 @@
+package HUAWEIOD;
+
+import java.util.Scanner;
+
+public class T01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+
+        String in = sc.nextLine();
+        String[] str = in.split(" ");
+        String result = str[str.length - 1];
+        System.out.println(result.length());
+
+
+    }
+}
---
+++ test/src/HUAWEIOD/T02.java
@@ -0,0 +1,8 @@
+package HUAWEIOD;
+
+public class T02 {
+    public static void main(String[] args) {
+        char c = '9';
+        System.out.println((int)c);
+    }
+}
